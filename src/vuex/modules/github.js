619a9b95f8d5688d670695fef18a46749fd2a652
xiaoluoboding <xiaoluoboding@gmail.com>
2016-09-14 09:35:21 +0800

    Finish render pertty tree

--- src/vuex/modules/github.js
+++ src/vuex/modules/github.js
@@ -3,19 +3,18 @@ import {
   SET_REPOLINK,
   SET_REPOTREE
 } from '../mutation-types'
-import {
-  filter
-} from 'lodash'
+import _ from 'lodash'
 import treeify from 'treeify'
 
 
 // initial state
 const state = {
   siteLink: 'https://github.com/',
-  repoLink: 'https://github.com/xiaoluoboding/vue-stroll',
+  repoLink: 'https://github.com/xiaoluoboding/vue-demo-collection',
   userName: '',
   repoName: '',
-  repoTree: []
+  repoTree: [],
+  rootTree: ''
 }
 
 // mutations
@@ -29,47 +28,49 @@ const mutations = {
     state.repoName = repoName
   },
   [SET_REPOTREE](state, repoTree) {
-    let perttyTree = {}
+    const self = this
 
-    // set rootTree
-    let rootTreeArray = filter(repoTree, (obj) => !obj.path.includes('/') && obj.type == 'tree')
-    let rootTree = {}
-    for (let i in rootTreeArray) {
-      if (rootTreeArray.hasOwnProperty(i)) {
-        rootTree[rootTreeArray[i].path] = []
-      }
+    // ES6 lambda calculus
+    const Y = f =>
+      (x => f(y => x(x)(y)))
+      (x => f(y => x(x)(y)))
+
+    // filter repoTree
+    const resolve = _.partial(_.map, _, 'path')
+    let rootTreeArray = resolve(_.filter(repoTree, (obj) => obj.type == 'tree'))
+    let otherBlobArray = resolve(_.filter(repoTree, (obj) => obj.path.includes('/') && obj.type == 'blob'))
+    let rootBlobArray = resolve(_.filter(repoTree, (obj) => !obj.path.includes('/') && obj.type == 'blob'))
+
+    // Build nesting directory
+    function parse(arr) {
+      return arr.reduceRight((result, key) => ({[key]: result}), null)
     }
 
-    // console.log(rootTree);
+    let treeArray = []
+    const walkPath = Y(
+      f => (rootTreeArray) => {
+        let fullPath = _.nth(rootTreeArray, 0)
+        let parsePath = _.split(fullPath, '/')
 
-    // set rootBlob
-    let rootBlobArray = filter(repoTree, (obj) => !obj.path.includes('/') && obj.type == 'blob')
-    let rootBlob = {}
-    for (let i in rootBlobArray) {
-      if (rootBlobArray.hasOwnProperty(i)) {
-        rootBlob[rootBlobArray[i].path] = null
+        treeArray.push(parse(parsePath))
+
+        rootTreeArray.shift()
+        _.size(rootTreeArray) == 0 ? 1 : f(rootTreeArray)
       }
-    }
+    )
+    walkPath(_.concat(rootTreeArray, otherBlobArray))
 
-    // console.log(rootBlob);
+    let rootTree = _.reduce(treeArray, function(result, value, key) {
+      return _.merge(result, value);
+    }, {});
 
-    // iterate childBlob
-    let childBlobArray = filter(repoTree, (obj) => obj.path.includes('/') && obj.type == 'blob')
-    for (var i in childBlobArray) {
-      if (childBlobArray.hasOwnProperty(i)) {
-        let treeParent = childBlobArray[i].path.split('\/').shift()
-        let treeChild = childBlobArray[i].path.split('\/').pop()
-        for (var i in rootTreeArray) {
-          if (rootTreeArray.hasOwnProperty(i) && rootTreeArray[i].path == treeParent) {
-            rootTree[treeParent][treeChild] = null
-          }
-        }
-      }
-    }
 
-    // set perttyTree
-    perttyTree = _.merge(rootTree, rootBlob)
-    console.log(perttyTree);
+    let rootBlob = _.reduce(rootBlobArray, function(result, value, key) {
+      return _.merge(result, {[value]: key})
+    }, {});
+
+    let perttyTree = _.merge(rootTree, rootBlob)
+
     state.repoTree = treeify.asTree(perttyTree)
   }
 }
